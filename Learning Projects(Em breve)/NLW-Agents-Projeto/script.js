b3cbe8d17c1c79a47c5422a078f5ad5c17bc0cc1
Ryan Markus <ryanmaciel480@gmail.com>
2025-10-01 11:58:26 -0300

    Update script.js
    Retirada da chave de API do Google.

--- Learning Projects(Em breve)/NLW-Agents-Projeto/script.js
+++ Learning Projects(Em breve)/NLW-Agents-Projeto/script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-// AIzaSyAhmAaaTLl2yuN-_FgRx2tJI-NsMwurtn0
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
